7a9b6aa053d11bd8f06eb756ff66c82d8c73b70f
slavaroskoshnyy <slava.roskoshnyy@gmail.com>
2023-07-22 17:55:33 +0300

    ferst commit

---
+++ js/common.js
@@ -0,0 +1,54 @@
+$(function () {
+  new WOW().init();
+
+  //   mnu mobile open
+
+  $(".hamburger").click(function () {
+    $(this).toggleClass("is-active");
+
+    if ($(this).hasClass("is-active")) {
+      $(".mnu_top").slideDown(300);
+    } else {
+      $(".mnu_top").slideUp(300);
+    }
+  });
+
+  //   mnu mobile open
+
+  // reviews tabs
+
+  $(".nav_rev>div").click(function () {
+    const revID = $(this).data("revid");
+
+    $(".nav_rev>div").not(this).removeClass("active");
+    $(this).addClass("active");
+
+    $(".item_rev").not(revID).removeClass("active");
+    $(revID).addClass("active");
+  });
+
+  // reviews tabs
+
+  //   yakor
+
+  $("a.yakor").on("click", function (event) {
+    var $anchor = $(this);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr("href")).offset().top - 57,
+        },
+        {
+          duration: 2000,
+          specialEasing: {
+            width: "linear",
+            height: "easeInOutCubic",
+          },
+        }
+      );
+    event.preventDefault();
+  });
+
+  //   yakor
+});
